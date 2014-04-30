96ab480dc97fdf39334306bb3b4bf892081ee0a6
ysc <yang-shangchuan@qq.com>
2014-04-30 20:42:00 +0800

    对文本进行分词的接口

---
+++ src/org/apdplat/evaluation/Segmenter.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.evaluation;
+
+/**
+ * 对文本进行分词的接口
+ * @author 杨尚川
+ */
+public interface Segmenter {
+    /**
+     * 对文本进行分词，词之间以空格分隔
+     * @param text 文本
+     * @return 分词后的文本
+     */
+    public String seg(String text);
+}
