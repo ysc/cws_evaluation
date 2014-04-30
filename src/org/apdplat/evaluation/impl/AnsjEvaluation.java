d36086d9e5b2e2a35c99e05246809d1b54921ef5
ysc <yang-shangchuan@qq.com>
2014-04-30 20:44:21 +0800

    Ansj分词器分词效果评估

---
+++ src/org/apdplat/evaluation/impl/AnsjEvaluation.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.evaluation.impl;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.BaseAnalysis;
+import org.ansj.splitWord.analysis.IndexAnalysis;
+import org.ansj.splitWord.analysis.NlpAnalysis;
+import org.ansj.splitWord.analysis.ToAnalysis;
+import org.apdplat.evaluation.Evaluation;
+import org.apdplat.evaluation.EvaluationResult;
+import org.apdplat.evaluation.Segmenter;
+
+/**
+ * Ansj分词器分词效果评估
+ * @author 杨尚川
+ */
+public class AnsjEvaluation extends Evaluation{
+    @Override
+    public List<EvaluationResult> run() throws Exception {
+        List<EvaluationResult> list = new ArrayList<>();
+        
+        list.add(run("BaseAnalysis"));        
+        list.add(run("ToAnalysis"));
+        list.add(run("NlpAnalysis"));
+        list.add(run("IndexAnalysis"));
+        
+        return list;
+    }
+    private EvaluationResult run(final String analysis) throws Exception{
+        // 对文本进行分词
+        String analyzer = "Ansj "+analysis;
+        switch(analysis){
+            case "BaseAnalysis":
+                    analyzer += " 基本分词";
+                    break;
+            case "ToAnalysis":
+                    analyzer += " 精准分词";
+                    break;
+            case "NlpAnalysis":
+                    try{
+                        analyzer += " NLP分词";
+                    }catch(Exception e){}
+                    break;
+            case "IndexAnalysis":
+                    analyzer += " 面向索引的分词";
+                    break;
+        }
+        String resultText = "temp/result-text-"+analysis+".txt";
+        float rate = segFile(testText, resultText, new Segmenter(){
+            @Override
+            public String seg(String text) {
+                StringBuilder result = new StringBuilder();
+                try{
+                    List<Term> terms = null;
+                    switch(analysis){
+                        case "BaseAnalysis":
+                                terms = BaseAnalysis.parse(text);
+                                break;
+                        case "ToAnalysis":
+                                terms = ToAnalysis.parse(text);
+                                break;
+                        case "NlpAnalysis":
+                                terms = NlpAnalysis.parse(text);
+                                break;
+                        case "IndexAnalysis":
+                                terms = IndexAnalysis.parse(text);
+                                break;
+                    }
+                    for(Term term : terms){
+                        result.append(term.getName()).append(" ");                    
+                    }                    
+                }catch(Exception e){}
+                return result.toString();
+            }
+        });
+        // 对分词结果进行评估
+        EvaluationResult result = evaluate(resultText, standardText);        
+        result.setSegSpeed(rate);
+        result.setAnalyzer(analyzer);
+        return result;
+    }
+    public static void main(String[] args) throws Exception{
+        List<EvaluationResult> list = new AnsjEvaluation().run();
+        //输出评估结果
+        Collections.sort(list);
+        System.out.println("******************************************************************************************************************");
+        int i=1;
+        for(EvaluationResult r : list){
+            System.out.println((i++)+"：");
+            System.out.println(r+"\n");
+        }
+        System.out.println("******************************************************************************************************************");
+    }
+}
