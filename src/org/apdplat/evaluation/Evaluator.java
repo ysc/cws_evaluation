f8040940a8a30de45fdb09e0e9262fc25313f3af
ysc <yang-shangchuan@qq.com>
2014-04-30 20:42:35 +0800

    中文分词器分词效果评估程序

---
+++ src/org/apdplat/evaluation/Evaluator.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.evaluation;
+
+import org.apdplat.evaluation.impl.MMSeg4jEvaluation;
+import org.apdplat.evaluation.impl.WordEvaluation;
+import org.apdplat.evaluation.impl.IKAnalyzerEvaluation;
+import org.apdplat.evaluation.impl.AnsjEvaluation;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 中文分词器分词效果评估程序
+ * @author 杨尚川
+ */
+public class Evaluator {
+    public static void main(String[] args) throws Exception{
+        List<EvaluationResult> list = new WordEvaluation().run();
+        list.addAll(new AnsjEvaluation().run());
+        list.addAll(new MMSeg4jEvaluation().run());
+        list.addAll(new IKAnalyzerEvaluation().run());
+        //输出评估结果
+        Collections.sort(list);
+        System.out.println("******************************************************************************************************************");
+        int i=1;
+        for(EvaluationResult r : list){
+            System.out.println((i++)+"：");
+            System.out.println(r+"\n");
+        }
+        System.out.println("******************************************************************************************************************");
+    }
+
+}
