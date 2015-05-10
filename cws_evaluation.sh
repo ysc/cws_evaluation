a3e4a0bde745f5f4c118d9a41de02f6b8efb3031
ysc <yang-shangchuan@qq.com>
2015-05-11 02:46:20 +0800

    删除旧脚本

--- cws_evaluation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-exec java -Xmx2400m -classpath ./cws_evaluation.jar:lib/word-1.1-SNAPSHOT.jar:lib/slf4j-api-1.6.4.jar:lib/ansj_seg-1.4.jar:lib/tree_split-1.2.jar:lib/mmseg4j-core-1.9.1.jar:lib/IKAnalyzer2012_u6.jar:lib/jcseg-core-1.9.2.jar:lib/fudannlp.jar:lib/trove.jar:lib/paoding-analysis-2.0.4-beta.jar:lib/lucene-core-2.4.1.jar:lib/commons-logging.jar:lib/jieba-analysis-0.0.2.jar:lib/stanford-segmenter-3.3.1.jar org.apdplat.evaluation.Evaluator
