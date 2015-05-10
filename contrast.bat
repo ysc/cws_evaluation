c36a7ab007e038c5c7c3682adbfa43078f85067b
ysc <yang-shangchuan@qq.com>
2015-05-11 02:28:12 +0800

    中文分词器分词效果评估程序

---
+++ contrast.bat
@@ -0,0 +1,7 @@
+echo 中文分词器分词效果对比程序
+echo 第一次启动非常慢，请耐心等待
+
+@echo off
+
+set JAVA_OPTS=-Xms3g -Xmx3g
+call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.WordSegmenter" -Dexec.args="utf-8"
