34e77d067bd2a91c50ac98464b745f0893876465
ysc <yang-shangchuan@qq.com>
2015-05-10 17:18:01 +0800

    word分词器分词效果评估

--- src/org/apdplat/evaluation/impl/WordEvaluation.java
+++ src/org/apdplat/evaluation/impl/WordEvaluation.java
@@ -21,14 +21,11 @@
 package org.apdplat.evaluation.impl;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import org.apdplat.evaluation.Evaluation;
 import org.apdplat.evaluation.EvaluationResult;
-import org.apdplat.evaluation.Segmenter;
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.segmentation.SegmentationAlgorithm;
-import org.apdplat.word.segmentation.Word;
 
 /**
  * word分词器分词效果评估
@@ -38,22 +35,18 @@ public class WordEvaluation extends Evaluation implements org.apdplat.evaluation
     @Override
     public List<EvaluationResult> run() throws Exception {
         List<EvaluationResult> list = new ArrayList<>();
-        SegmentationAlgorithm segmentationAlgorithm = SegmentationAlgorithm.FullSegmentation;
-        System.out.println("开始评估 word分词 "+segmentationAlgorithm.getDes());
+        for(SegmentationAlgorithm segmentationAlgorithm : SegmentationAlgorithm.values()){
+            System.out.println("开始评估 word分词 "+segmentationAlgorithm.getDes());
             list.add(run(segmentationAlgorithm));
             //每评估完一种算法就保存一次报告
             Evaluation.generateReport(list, "word分词器分词效果评估报告.txt");
+        }
         return list;
     }
-    private EvaluationResult run(final SegmentationAlgorithm segmentationAlgorithm) throws Exception{
+    private EvaluationResult run(SegmentationAlgorithm segmentationAlgorithm) throws Exception{
         //对文本进行分词
         String resultText = "temp/result-text-"+segmentationAlgorithm.name()+".txt";
-        float rate = segFile(testText, resultText, new Segmenter(){
-            @Override
-            public String seg(String text) {
-                return WordEvaluation.seg(text, segmentationAlgorithm);
-            }
-        });
+        float rate = segFile(testText, resultText, text -> WordEvaluation.seg(text, segmentationAlgorithm));
         //对分词结果进行评估
         EvaluationResult evaluationResult = evaluate(resultText, standardText);
         evaluationResult.setAnalyzer("word分词 "+segmentationAlgorithm.getDes());
@@ -70,9 +63,9 @@ public class WordEvaluation extends Evaluation implements org.apdplat.evaluation
     }
     private static String seg(String text, SegmentationAlgorithm segmentationAlgorithm) {
         StringBuilder result = new StringBuilder();
-        for(Word word : WordSegmenter.segWithStopWords(text, segmentationAlgorithm)){
-            result.append(word.getText()).append(" ");
-        }
+        WordSegmenter.segWithStopWords(text, segmentationAlgorithm)
+                .stream()
+                .forEach(word -> result.append(word.getText()).append(" "));
         return result.toString();
     }
     public static void main(String[] args) throws Exception{
