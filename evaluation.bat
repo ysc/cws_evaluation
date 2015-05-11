578eddaede7e2cb076a14b051d65d91523ba1657
ysc <yang-shangchuan@qq.com>
2015-05-12 07:51:43 +0800

    默认不评估stanford分词器，慢的离奇

--- evaluation.bat
+++ evaluation.bat
@@ -5,7 +5,7 @@ echo 第一次启动非常慢，请耐心等待
 
 set MAVEN_OPTS=-Xms3g -Xmx3g
 
-call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar"
+call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation"
 
 rem 如果需要排除评估某些分词器，则将下面分词器的名称作为参数传递给Evaluator类的main方法
 rem 如：
--- evaluation.sh
+++ evaluation.sh
@@ -5,7 +5,7 @@ echo 第一次启动非常慢，请耐心等待
 
 export MAVEN_OPTS="-Xms3g -Xmx3g"
 
-mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar"
+mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation"
 
 #如果需要排除评估某些分词器，则将下面分词器的名称作为参数传递给Evaluator类的main方法
 #如：
