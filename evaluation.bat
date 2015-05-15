bc23ff5816bd17905befe941ba127e8c4a3f396b
ysc <yang-shangchuan@qq.com>
2015-05-15 08:36:08 +0800

    支持纯速度测试

--- evaluation.bat
+++ evaluation.bat
@@ -1,15 +1,14 @@
 echo 中文分词器分词效果评估程序
-echo 第一次启动非常慢，请耐心等待
 
 @echo off
 
 set MAVEN_OPTS=-Xms3g -Xmx3g
 
-call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation"
+call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar -testText=data/test-text.txt -standardText=data/standard-text.txt  StanfordEvaluation"
 
 rem 如果需要排除评估某些分词器，则将下面分词器的名称作为参数传递给Evaluator类的main方法
 rem 如：
-rem call mvn -e clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation FudanNLPEvaluation"
+rem call mvn -e clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar -testText=data/test-text.txt -standardText=data/standard-text.txt  StanfordEvaluation FudanNLPEvaluation"
 rem WordEvaluation
 rem StanfordEvaluation
 rem SmartCNEvaluation
--- evaluation.sh
+++ evaluation.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 echo 中文分词器分词效果评估程序
-echo 第一次启动非常慢，请耐心等待
 
-export MAVEN_OPTS="-Xms3g -Xmx3g"
+export MAVEN_OPTS="-Xms4g -Xmx4g"
 
-mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation"
+mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar -testText=data/test-text.txt -standardText=data/standard-text.txt StanfordEvaluation"
 
 #如果需要排除评估某些分词器，则将下面分词器的名称作为参数传递给Evaluator类的main方法
 #如：
-#mvn -e clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar StanfordEvaluation FudanNLPEvaluation"
+#mvn -e clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.Evaluator" -Dexec.args="target/cws_evaluation-1.0.jar -testText=data/test-text.txt -standardText=data/standard-text.txt StanfordEvaluation FudanNLPEvaluation"
 #WordEvaluation
 #StanfordEvaluation
 #SmartCNEvaluation
