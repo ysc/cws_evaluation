2bca6ecc44bd4c9066b1d7209afa1e7a8d4ecf42
ysc <yang-shangchuan@qq.com>
2015-05-15 10:37:14 +0800

    最低4G内存设置

--- contrast.bat
+++ contrast.bat
@@ -3,6 +3,6 @@ echo 第一次启动非常慢，请耐心等待
 
 @echo off
 
-set MAVEN_OPTS=-Xms3g -Xmx3g
+set MAVEN_OPTS=-Xms4g -Xmx4g
 
 call mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.WordSegmenter" -Dexec.args="utf-8"
--- contrast.sh
+++ contrast.sh
@@ -3,6 +3,6 @@
 echo 中文分词器分词效果对比程序
 echo 第一次启动非常慢，请耐心等待
 
-export MAVEN_OPTS=" -Xms3g -Xmx3g "
+export MAVEN_OPTS=" -Xms4g -Xmx4g "
 
 mvn clean install exec:java -Dexec.mainClass="org.apdplat.evaluation.WordSegmenter" -Dexec.args="utf-8"
